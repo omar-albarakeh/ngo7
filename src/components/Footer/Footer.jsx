0875cf2b176c091890281ce6cd14596b71806176
omar albarakeh <omaralbarakeh2@gmail.com>
2025-04-17 20:16:08 +0300

    who we are  updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,24 +8,14 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <div className="footer-row">
-          {/* Logos and Socials */}
-          <div className="footer-col">
-            <div className="logos">
-              <img
-                src="/images/logo/logo1.png"
-                alt="Logo 1"
-                width="130"
-                height="60"
-              />
-              <img
-                src="/images/logo/logo2.png"
-                alt="Logo 2"
-                width="130"
-                height="60"
-              />
-            </div>
-            <div className="socials">
+        {/* Logos and Socials */}
+        <div className="footer-col logos-socials">
+          <div className="logos">
+            <img src="/images/logo/logo1.png" alt="Logo 1" />
+            <img src="/images/logo/logo2.png" alt="Logo 2" />
+          </div>
+          <div className="socials">
+            <div className="social-icons">
               <span className="follow-label">{t("footer.followUs")}</span>
               <a
                 href="https://www.facebook.com/sospalestinefr68/"
@@ -49,48 +39,43 @@ const Footer = () => {
                 href="https://www.youtube.com/@sospalestine68"
                 target="_blank"
                 rel="noopener noreferrer">
-                <img src="/images/Social-Media/youtube.png" alt="youtube" />
+                <img src="/images/Social-Media/youtube.png" alt="YouTube" />
               </a>
             </div>
           </div>
+        </div>
 
-          {/* Contact Info */}
-          <div className="footer-col">
-            <h5>{t("footer.contact")}</h5>
-            <ul className="contact-list">
-              <li>
-                <i className="fa fa-map-marker-alt"></i>
-                <a
-                  href="https://www.google.com/maps?q=17,+avenue+Aristide+Briand+-+68200+Mulhouse"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="contact-link">
-                  {t("footer.address")}
-                </a>
-              </li>
-              <li>
-                <i className="fa fa-envelope"></i>
-                <a
-                  href={`mailto:${t("footer.email")}`}
-                  className="contact-link">
-                  {t("footer.email")}
-                </a>
-              </li>
-            </ul>
-          </div>
+        {/* Contact Info */}
+        <div className="footer-col">
+          <h5>{t("footer.contact")}</h5>
+          <ul className="contact-list">
+            <li>
+              <i className="fa fa-map-marker-alt"></i>
+              <a
+                href="https://www.google.com/maps?q=17,+avenue+Aristide+Briand+-+68200+Mulhouse"
+                target="_blank"
+                rel="noopener noreferrer">
+                {t("footer.address")}
+              </a>
+            </li>
+            <li>
+              <i className="fa fa-envelope"></i>
+              <a href={`mailto:${t("footer.email")}`}>{t("footer.email")}</a>
+            </li>
+          </ul>
+        </div>
 
-          {/* Newsletter */}
-          <div className="footer-col">
-            <h5>{t("footer.newsletter.title")}</h5>
-            <p>{t("footer.newsletter.description")}</p>
-            <form className="newsletter-form">
-              <input
-                type="email"
-                placeholder={t("footer.newsletter.placeholder")}
-              />
-              <button type="submit">{t("footer.newsletter.button")}</button>
-            </form>
-          </div>
+        {/* Newsletter */}
+        <div className="footer-col">
+          <h5>{t("footer.newsletter.title")}</h5>
+          <p>{t("footer.newsletter.description")}</p>
+          <form className="newsletter-form">
+            <input
+              type="email"
+              placeholder={t("footer.newsletter.placeholder")}
+            />
+            <button type="submit">{t("footer.newsletter.button")}</button>
+          </form>
         </div>
       </div>
     </footer>
